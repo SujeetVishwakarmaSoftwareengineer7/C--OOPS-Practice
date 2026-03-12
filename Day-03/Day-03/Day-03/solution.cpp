9dd9096ba91c3c026aadf74f4fb3f779892828f0
Sujeet Vishwakarma <ssujeet6449@gmail.com>
2026-03-12 12:35:04 +0530

    Day-03 Added Learning
    Implement a simple class to sum two integers and print the result.

---
+++ Day-03/Day-03/Day-03/solution.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class sujeet {
+public:
+int a;
+int b;
+int sum;
+
+void print(){
+  cout<< a <<" " << b <<endl;
+cout<<sum<<endl;
+}
+};
+
+int main(){
+  sujeet s1;
+s.a = 10;
+s.b = 20;
+
+s1.sum = s.a + s1.b;
+cout<<" sum is "<<s1.sum<<endl;
+return 0;
+};
