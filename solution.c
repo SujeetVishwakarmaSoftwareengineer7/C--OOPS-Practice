e866e0e8a92ff9d1d58a1e5a8207eaff447b882f
Sujeet Vishwakarma <ssujeet6449@gmail.com>
2026-03-12 12:07:26 +0530

    Added Learning

---
+++ solution.c
@@ -0,0 +1,17 @@
+#include<iostream>
+  using namespace std;
+    int main(){
+    int n;
+         cout<<"enter the num"<<endl;
+         cin>>n;
+
+         int fact =1;
+          for(int i=1;i<=n;i++){
+           fact = fact*i;
+ 
+
+cout<<"factorial  is"<<fact<<endl;
+
+   }
+
+}
