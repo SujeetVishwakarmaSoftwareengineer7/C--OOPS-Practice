164f0480150e3338960167ce2258dead434b4036
Sujeet Vishwakarma <ssujeet6449@gmail.com>
2026-03-17 16:20:52 +0530

    Day-05 Added Learning

---
+++ Day-05/Day-05/solution.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+
+ class addition{
+    public:
+    int a;
+    int b;
+    int sum;
+
+     void main();
+    };
+
+    int main(){
+addition s1;
+s1.a=10;
+s1.b=20;
+s1.sum=s1.a+s1.b;
+cout<<"the sum of "<<s1.a<<" and "<<s1.b<<" is "<<s1.sum<<endl;
+    }
