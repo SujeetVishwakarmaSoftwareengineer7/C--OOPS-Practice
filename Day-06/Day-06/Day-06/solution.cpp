031496243bcc83497f33c295f85366e92a5537f5
Sujeet Vishwakarma <ssujeet6449@gmail.com>
2026-03-17 16:11:17 +0530

    Day-06 Added learning

---
+++ Day-06/Day-06/Day-06/solution.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+ 
+     int n;
+        cout<<"enter the value" <<endl;
+        cin>>n;
+
+
+        if(n>0){
+            cout<<"the number is positive"<<endl;
+        }
+        else if(n<0){
+            cout<<"the number is negative"<<endl;
+        }
+        else{
+            cout<<"the number is zero"<<endl;
+
+            
+
+}
+}
